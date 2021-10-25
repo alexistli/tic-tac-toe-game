b49192851dd08c4453b4ecf7b9aca9619f0222fa
alexistli <alexis.torelli.treanton@gmail.com>
2021-10-25 12:32:02 +0200

    Add player picking

--- src/tic_tac_toe_game/__main__.py
+++ src/tic_tac_toe_game/__main__.py
@@ -8,6 +8,11 @@ def main() -> None:
     """Tic Tac Toe Game."""
 
     click.secho("hello", fg="green")
+    if click.confirm("Do you want to play a game?", abort=True):
+        click.echo("Let's play a game...")
+
+    value = click.prompt('Please pick a player', type=click.Choice(["A", "B"], case_sensitive=False), default="A")
+    click.echo(value)
 
 
 if __name__ == "__main__":
