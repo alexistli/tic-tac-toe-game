709dec9f9c85239c3b5402502359bb4b96ba6711
alexistli <alexis.torelli.treanton@gmail.com>
2021-10-27 17:51:28 +0200

    Add NotAvailableError

--- src/tic_tac_toe_game/errors.py
+++ src/tic_tac_toe_game/errors.py
@@ -1,5 +1,5 @@
 """Error classes used in the project."""
-from typing import Tuple
+from typing import Tuple, Optional
 
 
 class OverwriteCellError(Exception):  # pragma: no cover
@@ -13,3 +13,18 @@ class OverwriteCellError(Exception):  # pragma: no cover
         """
         msg = f"Overwriting a non-empty cell is not allowed: cell at {coord}"
         super(OverwriteCellError, self).__init__(msg)
+
+
+class NotAvailableError(Exception):  # pragma: no cover
+    """Raised when conditions, to perform an operation, are not available."""
+
+    def __init__(self, message: Optional[str] = None) -> None:
+        """Constructor.
+
+        Args:
+            message: str, additional information about the error.
+        """
+        msg = f"Operation can't be performed as conditions are not available"
+        if message is not None:
+            msg += f": {message}"
+        super(NotAvailableError, self).__init__(msg)
