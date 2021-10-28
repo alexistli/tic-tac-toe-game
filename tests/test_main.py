57201e13e61f040d31f382d2c422b27da00883b6
alexistli <39305766+alexistli@users.noreply.github.com>
2021-10-28 13:23:31 +0200

    Revert "Refactoring adding tests, typing and documentation (#23)" (#24)
    This reverts commit f6b6f0c983d8a4e617c9cc3521f4c145d8c40ea7.

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,6 @@ import pytest
 from click.testing import CliRunner
 
 from tic_tac_toe_game import __main__
-from tic_tac_toe_game import engine
 
 
 @pytest.fixture
@@ -12,21 +11,7 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-# ================ Test CLI ================
-
-
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
-    assert result.exit_code == 1
-
-
-# ================ Test CLI ================
-
-
-def test_game_init_returns_game() -> None:
-    """It returns an object of type Game."""
-    player_a_mark = "X"
-    player_b_type = "H"
-    game = __main__.game_init(player_a_mark, player_b_type)
-    assert isinstance(game, engine.Game)
+    assert result.exit_code == 0
