d75dc5a33e6d8debf04e6e25ec07f496510012a8
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-16 11:24:55 +0100

    Add index page with starting button

--- src/app/main/routes.py
+++ src/app/main/routes.py
@@ -9,16 +9,24 @@ from app.main import bp
 
 @bp.route("/")
 def index():
+    """Shows the website index."""
+    return render_template("index.html", headline="Tic Tac Toe Game")
+
+
+@bp.route("/game")
+def game():
     """Shows the current game."""
     if "board" not in session:
         session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
         session["turn"] = "X"
 
-    return render_template("game.html", game=session["board"], turn=session["turn"])
+    return render_template(
+        "game.html", game=session["board"], turn=session["turn"], session=session
+    )
 
 
 @bp.route("/play/<int:row>/<int:col>")
 def play(row, col):
     """Gets the coordinates of the played cell."""
     session["board"][row][col] = session["turn"]
-    return redirect(url_for("main.index"))
+    return redirect(url_for("main.game"))
