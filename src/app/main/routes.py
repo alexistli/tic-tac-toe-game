b3524ebd44612a070254039cbb3d8411f5fbd0bd
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-17 14:04:13 +0100

    Update routes to have a fully working game

--- src/app/main/routes.py
+++ src/app/main/routes.py
@@ -18,40 +18,70 @@ def index():
 @bp.route("/game")
 def game():
     """Shows the current game."""
-    if "board" not in session:
+    if "game" not in session:
         current_game = engine.Engine("X", "B")
 
-        board = current_game.grid.grid
-
-        session["board"] = board
         session["game"] = current_game
-        session["turn"] = current_game.players_match.current().get_mark()
+        # session["turn"] = current_game.players_match.current().get_mark()
 
     current_game = session["game"]
-    session["turn"] = current_game.players_match.current().get_mark()
-    if session["turn"] == "O":
-        played_cell = current_game.grid.random_available_cell()
-        current_game.grid.set_cell(
-            coord=played_cell, value=session["turn"]  # type: ignore[arg-type]
-        )
-        current_game.players_match.switch()
-        session["turn"] = current_game.players_match.current().get_mark()
+    grid = current_game.grid
+    turn = current_game.players_match.current().get_mark()
 
-    return render_template(
-        "game.html", game=session["board"], turn=session["turn"], session=session
-    )
+    mark, played_cell = grid.get_last_play()
+    print("get_last_play:", mark, played_cell)
+    if not mark:
+        pass
+    elif grid.is_winning_move(played_cell, mark):  # type: ignore[arg-type]
+        del session["game"]
+        return redirect(url_for("main.win", mark=mark))
+    elif grid.is_full():
+        del session["game"]
+        return redirect(url_for("main.tie"))
+
+    # session["turn"] = current_game.players_match.current().get_mark()
+    if turn == "O":
+        chosen_cell = grid.random_available_cell()
+        row, col = chosen_cell
+        return redirect(url_for("main.play", row=row, col=col))
+
+        # grid.set_cell(coord=played_cell, value=turn)  # type: ignore[arg-type]
+        # current_game.players_match.switch()
+        # session["turn"] = current_game.players_match.current().get_mark()
+
+        # current_game.players_match.switch()
+
+    return render_template("game.html", board=grid.grid, turn=turn, session=session)
 
 
 @bp.route("/play/<int:row>/<int:col>")
 def play(row, col):
     """Gets the coordinates of the played cell."""
-    session["board"][row][col] = session["turn"]
+    # session["board"][row][col] = session["turn"]
 
     current_game = session["game"]
+    grid = current_game.grid
+    turn = current_game.players_match.current().get_mark()
+
+    grid.set_cell(coord=(row, col), value=turn)  # type: ignore[arg-type]
+
+    # current_game.players_match.switch()
+    # session["turn"] = current_game.players_match.current().get_mark()
     current_game.players_match.switch()
-    session["turn"] = current_game.players_match.current().get_mark()
 
     return redirect(url_for("main.game"))
+
+
+@bp.route("/win/<string:mark>")
+def win(mark):
+    """Announces the winning player."""
+    return render_template("win.html", mark=mark)
+
+
+@bp.route("/tie")
+def tie():
+    """Announces players are tied."""
+    return render_template("tie.html")
 
 
 @socketio.on("my event")
