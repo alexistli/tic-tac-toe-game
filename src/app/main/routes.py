dec7c54aa2992e95706af6e869451f7448035fa1
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-21 11:07:32 +0100

    Refactor to make routes compatible with new grid

--- src/app/main/routes.py
+++ src/app/main/routes.py
@@ -1,18 +1,15 @@
 """Flask app routes."""
-import numpy as np
 from flask import redirect
 from flask import render_template
+from flask import request
 from flask import session
 from flask import url_for
-from mctspy.tree.nodes import TwoPlayersGameMonteCarloTreeSearchNode
-from mctspy.tree.search import MonteCarloTreeSearch
 
 from app import socketio
 from app.main import bp
 from tic_tac_toe_game import engine
 from tic_tac_toe_game.engine import Grid
-from tic_tac_toe_game.mcts_ai import MARKING_CORRESPONDENCE
-from tic_tac_toe_game.mcts_ai import TicTacToeGameState
+from tic_tac_toe_game.mcts_ai import compute_best_move
 
 
 @bp.route("/")
@@ -21,58 +18,37 @@ def index():
     return render_template("index_socket.html", headline="Tic Tac Toe Game")
 
 
-@bp.route("/game")
+@bp.route("/game", methods=["GET", "POST"])
 def game():
     """Shows the current game."""
-    if "game" not in session:
-        current_game = engine.Engine("X", "B")
-        current_game.grid = Grid()
-
-        session["game"] = current_game
-        # session["turn"] = current_game.players_match.current().get_mark()
-
     current_game = session["game"]
     grid = current_game.grid
     turn = current_game.players_match.current().get_mark()
+    chosen_cell = None
 
-    mark, played_cell = grid.get_last_play()
-    print("get_last_play:", mark, played_cell)
-    if not mark:
+    if "choice" in request.form:
+        row_str, col_str = request.form["choice"].split()
+        chosen_cell = int(row_str), int(col_str)
+
+        grid.set_cell(coord=chosen_cell, value=turn)  # type: ignore[arg-type]
+
+        current_game.players_match.switch()
+
+        if not grid.is_full():
+            turn = current_game.players_match.current().get_mark()
+            list_grid = current_game.grid.dump_to_int_array()
+            chosen_cell = compute_best_move(turn, list_grid)
+
+            grid.set_cell(coord=chosen_cell, value=turn)  # type: ignore[arg-type]
+
+            current_game.players_match.switch()
+
+    if chosen_cell is None:
         pass
-    elif grid.is_winning_move(played_cell, mark):  # type: ignore[arg-type]
-        del session["game"]
-        return redirect(url_for("main.win", mark=mark))
+    elif grid.is_winning_move(chosen_cell, turn):  # type: ignore[arg-type]
+        return redirect(url_for("main.win", mark=turn))
     elif grid.is_full():
-        del session["game"]
         return redirect(url_for("main.tie"))
-
-    # session["turn"] = current_game.players_match.current().get_mark()
-    if turn == "O":
-        next_to_move = MARKING_CORRESPONDENCE[turn]
-
-        state = np.array(current_game.grid.dump_to_int_array())
-        initial_board_state = TicTacToeGameState(state=state, next_to_move=next_to_move)
-        root = TwoPlayersGameMonteCarloTreeSearchNode(state=initial_board_state)
-        mcts = MonteCarloTreeSearch(root)
-        best_node = mcts.best_action(10000)
-
-        print("\nSub:")
-        sub = best_node.state.board - best_node.parent.state.board
-        print(sub)
-
-        x_coords, y_coords = np.where(sub == next_to_move)
-        print(x_coords, y_coords)
-        print(x_coords[0], y_coords[0])
-        chosen_cell = (x_coords[0], y_coords[0])
-
-        row, col = chosen_cell
-        return redirect(url_for("main.play", row=row, col=col))
-
-        # grid.set_cell(coord=played_cell, value=turn)  # type: ignore[arg-type]
-        # current_game.players_match.switch()
-        # session["turn"] = current_game.players_match.current().get_mark()
-
-        # current_game.players_match.switch()
 
     return render_template("game.html", board=grid.grid, turn=turn, session=session)
 
@@ -105,6 +81,17 @@ def win(mark):
 def tie():
     """Announces players are tied."""
     return render_template("tie.html")
+
+
+@bp.route("/new_game")
+def new_game():
+    """Initializes a new game."""
+    current_game = engine.Engine("X", "B")
+    current_game.grid = Grid()
+
+    session["game"] = current_game
+
+    return redirect(url_for("main.game"))
 
 
 @socketio.on("my event")
