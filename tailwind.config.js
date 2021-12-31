89a99824840755f63b0cf0dbe3ce2acab2e62f46
alexistli <alexis.torelli.treanton@gmail.com>
2021-12-31 01:37:22 +0100

    Add grid borders and remove background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        "1/2": "50%",
+        full: "100%",
+      },
+    },
   },
   variants: {
     extend: {},
