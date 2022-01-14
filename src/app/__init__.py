c9aca074ebcef6a413234d28ba6f93f6d144dabf
alexistli <alexis.torelli.treanton@gmail.com>
2022-01-14 19:52:29 +0100

    Update to match new logging setup

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,7 @@
 """Application factory."""
-import logging
 from typing import Type
 
+import structlog
 from config import Config
 from flask import Flask
 from flask_assets import Bundle
@@ -15,18 +15,13 @@ ASYNC_MODE = "eventlet"
 session = Session()
 socketio = SocketIO()
 
-logger = logging.getLogger("app")  # logger configured in gunicorn.conf.py
+logger = structlog.get_logger()  # logger configured in logging_setup.py
 
 
 def create_app(config_class: Type[Config] = Config) -> Flask:
     """Creates the core application."""
     app: Flask = Flask(__name__)
     app.config.from_object(config_class)
-
-    # attach the logger and its handlers to the Flask app
-    app.logger.handlers = logger.handlers
-    app.logger.setLevel(logger.level)
-    logger.info("Flask game")
 
     session.init_app(app)
     socketio.init_app(app, async_mode=ASYNC_MODE, manage_session=False, logger=logger)
