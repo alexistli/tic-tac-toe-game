9324144cc9f460788213bc9f71e569ea68ee9318
alexistli <alexis.torelli.treanton@gmail.com>
2022-01-26 22:18:03 +0100

    Refactor to use Move object instead of passing coordinates values

--- src/app/main/events.py
+++ src/app/main/events.py
@@ -1,6 +1,4 @@
 """Socket-IO events."""
-from typing import Tuple
-
 import structlog
 from flask import session
 from flask_socketio import emit
@@ -26,14 +24,15 @@ def join(message):
 def move_received(data):
     """TODO."""
     logger.debug(f"socket.move: {data}")
-    chosen_cell = parse_raw_move(data["coord"])
+    move = engine.Move(**data["move"])
+
+    logger.debug(move)
 
     room = session["room"]
     current_game = state.get_state(room)
     logger.debug("before persist_move", game=current_game)
 
-    current_mark = current_game.players_match.current().get_mark()
-    current_game.board.set_cell(coord=chosen_cell, value=current_mark)
+    current_game.board.make_move(move)
     current_game.players_match.switch()
 
     state.set_state(room, current_game)
@@ -43,23 +42,3 @@ def move_received(data):
         to=data["room"],
         include_self=True,
     )
-
-
-def parse_raw_move(raw_move: str) -> Tuple[int, int]:
-    """TODO."""
-    logger.debug(f"raw_move: {raw_move}")
-    row_str, col_str = raw_move.split()
-    logger.debug(f"row_str, col_str: {row_str} {col_str}")
-    parsed_move = int(row_str), int(col_str)
-    return parsed_move
-
-
-def persist_move(game: engine.Engine, cell: Tuple[int, int]) -> None:
-    """TODO."""
-    logger.debug("before persist_move", game=game)
-
-    current_mark = game.players_match.current().get_mark()
-    game.board.set_cell(coord=cell, value=current_mark)
-    game.players_match.switch()
-
-    logger.debug("after persist_move", game=game, session=session)
