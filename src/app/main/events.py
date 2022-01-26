9a351d3dd8eac1e7afde6f8f255c15abeb6df4d0
alexistli <alexis.torelli.treanton@gmail.com>
2022-01-26 22:31:19 +0100

    Remove use of session for event and small improvements

--- src/app/main/events.py
+++ src/app/main/events.py
@@ -23,19 +23,12 @@ def join(message):
 @socketio.event
 def move_received(data):
     """TODO."""
-    logger.debug(f"socket.move: {data}")
-    move = engine.Move(**data["move"])
+    logger.debug("move_received with data", data=data)
 
-    logger.debug(move)
-
-    room = session["room"]
-    current_game = state.get_state(room)
-    logger.debug("before persist_move", game=current_game)
-
-    current_game.board.make_move(move)
+    current_game = state.get_state(data["room"])
+    current_game.board.make_move(engine.Move(**data["move"]))
     current_game.players_match.switch()
-
-    state.set_state(room, current_game)
+    state.set_state(data["room"], current_game)
 
     emit(
         "refresh_game",
