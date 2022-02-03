7b7498bad353faa499a7f95df307bd292b5370cd
alexistli <alexis.torelli.treanton@gmail.com>
2022-02-03 20:41:07 +0100

    Improve documentation

--- src/config.py
+++ src/config.py
@@ -4,15 +4,14 @@ from tempfile import mkdtemp
 
 
 class Config:
-    """Base config, uses local staging database."""
+    """Base config."""
 
     # General Config
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    STATIC_FOLDER = f"{os.environ.get('APP_FOLDER')}/static/dist"
 
     # Flask-Session
     SESSION_FILE_DIR = mkdtemp()
     SESSION_PERMANENT = True
     SESSION_TYPE = "filesystem"
     SESSION_COOKIE_HTTPONLY = False
-
-    STATIC_FOLDER = f"{os.environ.get('APP_FOLDER')}/static/dist"
