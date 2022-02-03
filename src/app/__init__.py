d0c0768c410770a28ec23e473b98202cbca0dd68
alexistli <alexis.torelli.treanton@gmail.com>
2022-02-03 20:58:58 +0100

    Improve documentation and better structure

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -18,30 +18,37 @@ ASYNC_MODE = "eventlet"
 
 session = Session()
 socketio = SocketIO()
+assets = Environment()
 
 logger = structlog.get_logger()  # logger configured in logging_setup.py
 
 
 def create_app(config_class: Type[Config] = Config) -> Flask:
-    """Creates the core application."""
+    """Creates and configure an instance of the Flask application."""
     app: Flask = Flask(__name__)
     app.config.from_object(config_class)
 
     session.init_app(app)
     socketio.init_app(app, async_mode=ASYNC_MODE, manage_session=False, logger=logger)
 
-    assets = Environment(app)
-
-    css = Bundle("src/main.css", output="dist/main.css", filters="postcss")
-    js = Bundle("src/*.js", output="dist/main.js")
-    assets.register("css", css)
-    assets.register("js", js)
-
-    css.build()
-    js.build()
+    assets.init_app(app)
+    with app.app_context():
+        _compile_static_assets(assets)
 
     from app.main import bp as main_bp
 
     app.register_blueprint(main_bp)
 
     return app
+
+
+def _compile_static_assets(assets_):
+    """Configures and builds asset bundles."""
+    css = Bundle("src/main.css", output="dist/main.css", filters="postcss")
+    js = Bundle("src/*.js", output="dist/main.js")
+
+    assets.register("css", css)
+    assets.register("js", js)
+
+    css.build()
+    js.build()
